b97421eae82abc8ac92d372c814cfebe7c8e2e17
Rafael Tivane <luislangabusiness@gmail.com>
2022-02-10 14:38:04 +0200

    creating tablet page

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,40 @@
-const { dest, src, series, watch} = require('gulp');
+const { dest, src, parallel, series, watch } = require('gulp');
+const cleanCSS = require('gulp-clean-css');
+const gulpClean = require('gulp-clean');
+const gulpHtmlMin = require('gulp-htmlmin');
+//const gulpImageMin = require('gulp-imagemin');
 
+
+const clean = () => {
+    return src(['dist'])
+        .pipe(gulpClean({ read: false }))
+
+};
+
+const cssClean = () => {
+    return src(['dist/**/*.css', '!node_module/**', '!src/**/*.css'])
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(dest('dist'))
+};
+
+
+const minifyHtml = () => {
+    return src('dist/*.html')
+        .pipe(gulpHtmlMin({ collapseWhitespace: true }))
+        .pipe(dest('dist'));
+};
+
+/*const minifyImages = () => {
+    return src('dist/assets/images')
+        .pipe(gulpImageMin())
+        .pipe(dest('dist'))
+}*/
 
 const copy = () => {
-    return src(['src/**/*.html', 'src/**/*.svg', '!node_module/**', 'src/**/*.css', '!src/assets/css/tailwind.css'])
+    return src(['src/**/*.html', 'src/**/*.svg', '!node_module/**', 'src/**/*.css', '!src/assets/css/input.css'])
         .pipe(dest('dist'))
 };
 
 exports.default = function() {
-    watch('src/**/*', series(copy));
+    watch('src/**/*', series(clean, copy, parallel(cssClean, minifyHtml)));
 };
