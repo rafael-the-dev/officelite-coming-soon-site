c21db782989d83e6bde263820376a2d0372996a2
Rafael Tivane <luislangabusiness@gmail.com>
2022-02-11 11:47:27 +0200

    adding footer

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ const cleanCSS = require('gulp-clean-css');
 const gulpClean = require('gulp-clean');
 const gulpHtmlMin = require('gulp-htmlmin');
 //const gulpImageMin = require('gulp-imagemin');
+const gulpUseMin = require('gulp-usemin');
+const rev = require('gulp-rev');
 
 
 const clean = () => {
@@ -28,7 +30,17 @@ const minifyHtml = () => {
     return src('dist/assets/images')
         .pipe(gulpImageMin())
         .pipe(dest('dist'))
-}*/
+};*/
+
+const usemin = () => {
+    return src(['dist/**/*.html', '!node_module/**', '!src/**'])
+        .pipe(gulpUseMin({
+            css: [ rev() ],
+            html: [ gulpHtmlMin({ collapseWhitespace: true }) ],
+            //inlinecss: [ cleanCSS({compatibility: 'ie8'}), 'concat' ]
+        }))
+        .pipe(dest('build/'));
+};
 
 const copy = () => {
     return src(['src/**/*.html', 'src/**/*.svg', '!node_module/**', 'src/**/*.css', '!src/assets/css/input.css'])
@@ -36,5 +48,5 @@ const copy = () => {
 };
 
 exports.default = function() {
-    watch('src/**/*', series(clean, copy, parallel(cssClean, minifyHtml)));
+    watch('src/**/*', series(clean, copy, parallel(minifyHtml, cssClean)));
 };
