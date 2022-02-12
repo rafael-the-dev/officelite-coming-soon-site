06768eeda57fafc6118131056539a795a6d2ee08
Rafael Tivane <luislangabusiness@gmail.com>
2022-02-12 14:11:10 +0200

    adding event handler to list item

--- src/assets/js/signUp.js
+++ src/assets/js/signUp.js
@@ -1,14 +1,30 @@
 let dropDownList;
 let dropdownButton;
+let selectedItem;
+
+const listKeys = {
+    'basic-pack': '<span class="blue-color font-bold capitalize">Basic pack</span> Free',
+    'pro-pack': '<span class="blue-color font-bold capitalize">Pro ack</span> Free',
+    'ultimate-pack': '<span class="blue-color font-bold capitalize">Ultimate pack</span> Free'
+}
 
 const buttonClickHandler = () => {
     dropDownList.classList.toggle('hidden');
 };
 
+const listItemClickHandler = prop => () => {
+    selectedItem.innerHTML = listKeys[prop];
+    dropDownList.classList.add('hidden');
+};
+
 window.addEventListener('load', () => {
-    dropDownList = document.querySelector('[data-dropdown-list]');
+    dropDownList = document.querySelector('[data-dropdown-list]');//
     dropdownButton = document.querySelector('[data-dropdown-toggle]');
-    console.log(dropdownButton)
+    selectedItem = document.querySelector('[data-dropdown-selected-item]');
+    console.log(selectedItem)
 
     dropdownButton.addEventListener('click', buttonClickHandler)
+    document.querySelectorAll('[data-dropdown-list-item]').forEach(element => {
+        element.addEventListener('click', listItemClickHandler(element.getAttribute('data-dropdown-list-item')))
+    })
 })
