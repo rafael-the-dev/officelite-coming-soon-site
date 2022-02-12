1163052ab2849733d4e948e6acafb53b76b12449
Rafael Tivane <luislangabusiness@gmail.com>
2022-02-12 13:58:42 +0200

    adding event handler to button, to toggle list visibility

--- src/assets/js/signUp.js
+++ src/assets/js/signUp.js
@@ -1,9 +1,14 @@
 let dropDownList;
 let dropdownButton;
 
+const buttonClickHandler = () => {
+    dropDownList.classList.toggle('hidden');
+};
 
 window.addEventListener('load', () => {
     dropDownList = document.querySelector('[data-dropdown-list]');
     dropdownButton = document.querySelector('[data-dropdown-toggle]');
     console.log(dropdownButton)
+
+    dropdownButton.addEventListener('click', buttonClickHandler)
 })
