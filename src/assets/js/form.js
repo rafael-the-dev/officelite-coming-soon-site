971e5cd92396b2c435cef1946434adc9c6ea274e
Rafael Tivane <luislangabusiness@gmail.com>
2022-02-13 08:49:41 +0200

    validating form

--- src/assets/js/form.js
+++ src/assets/js/form.js
@@ -2,10 +2,34 @@ let nameInput;
 let emailInput;
 let phoneNumberInput;
 let companyInput;
+let isValidForm = false;
 
 const submitHandler = event => {
     event.preventDefault();
     console.log('hello')
+
+    const name = nameInput.value.trim();
+    const email = nameInput.value.trim();
+    const phoneNumber = nameInput.value.trim();
+    const company = nameInput.value.trim();
+
+    if(!Boolean(name)) {
+        isValidForm = false;
+    }
+
+    if(!Boolean(email)) {
+        isValidForm = false;
+    }
+
+    if(!Boolean(phoneNumber)) {
+        isValidForm = false;
+    }
+
+    if(!Boolean(company)) {
+        isValidForm = false;
+    }
+
+    console.log('is valid form', isValidForm);
 };
 
 const start = () => { 
@@ -13,8 +37,9 @@ const start = () => {
     emailInput = document.querySelector('[data-input-email]');
     phoneNumberInput = document.querySelector('[data-input-phone-number]');
     companyInput = document.querySelector('[data-input-company]');
-    document.querySelector('[data-form]').addEventListener('submit', submitHandler);
-    console.log(document.querySelector('[data-form]'))
+
+    const form = document.querySelector('[data-form]');
+    form.addEventListener('submit', submitHandler);
 };
 
 window.addEventListener('load', start);
